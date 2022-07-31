301cbabf62892ec23e2d4f96510aa8b72a3fd0bd
Mariana Leite <mariana.silva@vmis.com.br>
2022-07-31 10:38:43 -0300

    create tests structure

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-echo "Building CppChallenge..."
+if [ $# -lt 1 ]; then
+    echo "Please, run # build.sh <TARGET>"
+    echo "    TARGET - Target of build e.g.: CppChallenge, CSVFileTests, etc"
+    echo "Example: build.sh CppChallenge"
+    exit 1
+fi
+
+echo "Building $1..."
 
 BUILD_DIR="build"
 
 cmake -B ${BUILD_DIR}
-cmake --build ${BUILD_DIR}
+cmake --build ${BUILD_DIR} --target $1
---
+++ tests/college_database_tests/college_database_tests.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file college_database_tests.cpp
+ * @author Mariana Leite
+ * @brief 
+ * @version 0.1
+ * @date 2022-07-31
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
