d46f8caf830e5ffffaa8564368e0c0fed20b6c90
Bruno Lebtag <brlebtag@gmail.com>
2022-11-02 14:19:31 -0300

    Implement to_csv() and to_string() in order to have a string representation in free or csv format.

--- school.h
+++ school.h
@@ -2,6 +2,7 @@
 #define __SCHOOL_H__
 
 #include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -15,10 +16,13 @@ private:
     int _critical_reading_mean = NOT_PRESENT;
     int _mathematics_mean = NOT_PRESENT;
     int _writing_mean = NOT_PRESENT;
+    string get_mean(int mean);
     school(string dbn, string name, int number_test_takers, int critical_reading_mean, int mathematics_mean, int writing_mean);
 
 public:    
     ~school();
+    string to_string();
+    string to_csv();
     school* create(string data);
 };
 
