12b6aadd9d841f2b0daa8042312e846e22b6ffd6
Henrique Daniel Hamisch <henrique.daniel@khomp.com>
2022-12-12 21:10:43 -0300

    Add basic class to hold CSV record data, i.e. a school test result

---
+++ include/SchoolTestResult.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+
+class SchoolTestResult {
+public:
+    std::string dbn;
+    std::string schoolName;
+    unsigned int numberOfTestTakers;
+    unsigned int criticalReadingMean;
+    unsigned int mathematicsMean;
+    unsigned int writingMean;
+
+    friend std::ostream& operator<<(std::ostream& out, SchoolTestResult const& result) {
+        return out << "Results for " << result.schoolName << "(" << result.dbn << ")" << std::endl <<
+               "Number of test takers: " << result.numberOfTestTakers << std::endl <<
+               "Critical Reading Mean: " << result.criticalReadingMean << std::endl <<
+               "Mathematics Mean: " << result.mathematicsMean << std::endl <<
+               "Writing Mean: " << result.writingMean << std::endl;
+    }
+};
