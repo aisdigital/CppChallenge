fca3839ac793624ddf812d37b346c37b590553a8
Henrique Daniel Hamisch <henrique.daniel@khomp.com>
2022-12-13 22:11:29 -0300

    Change class storage for data to unordered_map to favor search by DBN and refactor accordingly

---
+++ include/SatResult.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+
+class SatResult {
+public:
+    std::string schoolName;
+    std::string numberOfTestTakers;
+    std::string criticalReadingMean;
+    std::string mathematicsMean;
+    std::string writingMean;
+
+    friend std::ostream& operator<<(std::ostream& out, SatResult &entry) {
+        return out << "Results for " << entry.schoolName << std::endl <<
+               "Number of test takers: " << entry.numberOfTestTakers << std::endl <<
+               "Critical Reading Mean: " << entry.criticalReadingMean << std::endl <<
+               "Mathematics Mean: " << entry.mathematicsMean << std::endl <<
+               "Writing Mean: " << entry.writingMean << std::endl;
+    }
+};
