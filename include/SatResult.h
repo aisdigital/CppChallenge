b249ed40bd697b2921c08b46608a81f6a3f42334
Henrique Daniel Hamisch <henrique.daniel@khomp.com>
2022-12-13 22:15:19 -0300

    Improve pretty printing of SatResult class

--- include/SatResult.h
+++ include/SatResult.h
@@ -11,10 +11,11 @@ public:
     std::string writingMean;
 
     friend std::ostream& operator<<(std::ostream& out, SatResult &entry) {
-        return out << "Results for " << entry.schoolName << std::endl <<
-               "Number of test takers: " << entry.numberOfTestTakers << std::endl <<
-               "Critical Reading Mean: " << entry.criticalReadingMean << std::endl <<
-               "Mathematics Mean: " << entry.mathematicsMean << std::endl <<
-               "Writing Mean: " << entry.writingMean << std::endl;
+        return out << "SAT Results for \"" << entry.schoolName << "\"" << std::endl
+                   << "----------------------------------------------------------------------" << std::endl
+                   << "Number of test takers: " << entry.numberOfTestTakers << std::endl
+                   << "Critical Reading Mean: " << entry.criticalReadingMean << std::endl
+                   << "Mathematics Mean: " << entry.mathematicsMean << std::endl
+                   << "Writing Mean: " << entry.writingMean << std::endl;
     }
 };
